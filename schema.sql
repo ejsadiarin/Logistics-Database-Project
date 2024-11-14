12b343ab7c3494758b5b9ea7d2f21869275242ce
ejsadiarin <kyube.ejs@proton.me>
2024-11-14 19:02:02 +0800

    feat(schema): sql schema tables now working, just import it

--- schema.sql
+++ schema.sql
@@ -1,24 +1,27 @@
-CREATE DATABASE ccinfom;
+CREATE DATABASE IF NOT EXISTS `ccinfom`;
 
-USE ccinfom;
+USE `ccinfom`;
 
+DROP TABLE IF EXISTS `driver`;
 CREATE TABLE driver (
     driver_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(255) NOT NULL,
     rate DECIMAL(10,2) NOT NULL,
     contact_number VARCHAR(15) NOT NULL,
-    status ENUM('AVAILABLE','IN TRANSIT', 'ON LEAVE', 'UNAVAILABLE') NOT NULL
+    status ENUM('AVAILABLE','IN TRANSIT', 'ON LEAVE', 'UNAVAILABLE') DEFAULT 'AVAILABLE'
 );
 
+DROP TABLE IF EXISTS `vehicle`;
 CREATE TABLE vehicle (
 	vehicle_id INT PRIMARY KEY,
     plate_number VARCHAR(6) NOT NULL,
     fuel_economy FLOAT NOT NULL,
     last_maintenance_date DATETIME NOT NULL,
     max_load_weight FLOAT NOT NULL,
-    status ENUM('AVAILABLE', 'IN TRANSIT', 'NEEDS MAINTENANCE', 'UNAVAILABLE') NOT NULL
+    status ENUM('AVAILABLE', 'IN TRANSIT', 'NEEDS MAINTENANCE', 'UNAVAILABLE') DEFAULT 'AVAILABLE'
 );
 
+DROP TABLE IF EXISTS `customer`;
 CREATE TABLE customer (
     customer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     company_name VARCHAR(255) NOT NULL,
@@ -26,20 +29,10 @@ CREATE TABLE customer (
     company_contract VARCHAR(255) NOT NULL,
     billing_address VARCHAR(255) NOT NULL,
     amount_paid DECIMAL(10, 2) NOT NULL,
-    date_paid DATETIME NOT NULL,
-    request_id INT,
-    FOREIGN KEY (request_id) REFERENCES request(request_id)
+    date_paid DATETIME NOT NULL
 );
 
-CREATE TABLE logistics (
-    logisticsID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    distance DECIMAL(10, 2) NOT NULL,
-    normalCost DECIMAL(10, 2) NOT NULL,
-    status ENUM('ARRIVED', 'IN TRANSIT', 'CANCELLED', 'PENDING') NOT NULL,
-    schedule_id INT,
-    FOREIGN KEY (schedule_id) REFERENCES schedule(schedule_id)
-);
-
+DROP TABLE IF EXISTS `request`;
 CREATE TABLE request (
     request_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     requested_date DATETIME NOT NULL,
@@ -51,15 +44,24 @@ CREATE TABLE request (
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
 
+DROP TABLE IF EXISTS `schedule`;
 CREATE TABLE schedule (
     schedule_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     date DATETIME NOT NULL,
     driver_id INT,
     vehicle_id INT,
     request_id INT,
-    logistics_id INT,
     FOREIGN KEY (driver_id) REFERENCES driver(driver_id),
     FOREIGN KEY (vehicle_id) REFERENCES vehicle(vehicle_id),
-    FOREIGN KEY (request_id) REFERENCES request(request_id),
-    FOREIGN KEY (logistics_id) REFERENCES logistics(logistics_id)
+    FOREIGN KEY (request_id) REFERENCES request(request_id)
+);
+
+DROP TABLE IF EXISTS `logistics`;
+CREATE TABLE logistics (
+    logisticsID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    distance DECIMAL(10, 2) NOT NULL,
+    normalCost DECIMAL(10, 2) NOT NULL,
+    status ENUM('ARRIVED', 'IN TRANSIT', 'CANCELLED', 'PENDING') DEFAULT 'PENDING',
+    schedule_id INT,
+    FOREIGN KEY (schedule_id) REFERENCES schedule(schedule_id)
 );
