7484225b47ec9e0616211496c9cd33f815c40aed
ejsadiarin <kyube.ejs@proton.me>
2024-11-18 16:53:46 +0800

    refactor(sql): proper schema table creation for idempotent imports

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
-CREATE DATABASE IF NOT EXISTS `ccinfom`;
+SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE DATABASE IF NOT EXISTS `ccinfom`;
 USE `ccinfom`;
 
 DROP TABLE IF EXISTS `drivers`;
@@ -9,17 +10,17 @@ CREATE TABLE drivers (
     rate DECIMAL(10,2) NOT NULL,
     contact_number VARCHAR(15) NOT NULL,
     status ENUM('AVAILABLE','IN_TRANSIT', 'ON_LEAVE', 'UNAVAILABLE') DEFAULT 'AVAILABLE'
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `vehicles`;
 CREATE TABLE vehicles (
-	vehicle_id INT PRIMARY KEY,
+    vehicle_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     plate_number VARCHAR(6) NOT NULL,
     fuel_economy FLOAT NOT NULL,
     last_maintenance_date DATE NOT NULL,
     max_load_weight FLOAT NOT NULL,
     status ENUM('AVAILABLE', 'IN_TRANSIT', 'NEEDS_MAINTENANCE', 'UNAVAILABLE') DEFAULT 'AVAILABLE'
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `customers`;
 CREATE TABLE customers (
@@ -30,7 +31,7 @@ CREATE TABLE customers (
     billing_address VARCHAR(255) NOT NULL,
     amount_paid DECIMAL(10, 2) NOT NULL,
     date_paid DATE NOT NULL
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `requests`;
 CREATE TABLE requests (
@@ -42,7 +43,7 @@ CREATE TABLE requests (
     load_weight DECIMAL(10, 2) NOT NULL,
     customer_id INT,
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `schedules`;
 CREATE TABLE schedules (
@@ -54,7 +55,7 @@ CREATE TABLE schedules (
     FOREIGN KEY (driver_id) REFERENCES drivers(driver_id),
     FOREIGN KEY (vehicle_id) REFERENCES vehicles(vehicle_id),
     FOREIGN KEY (request_id) REFERENCES requests(request_id)
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `logistics`;
 CREATE TABLE logistics (
@@ -64,4 +65,6 @@ CREATE TABLE logistics (
     status ENUM('ARRIVED', 'IN_TRANSIT', 'CANCELLED', 'PENDING') DEFAULT 'PENDING',
     schedule_id INT,
     FOREIGN KEY (schedule_id) REFERENCES schedules(schedule_id)
-);
+) ENGINE=InnoDB;
+
+SET FOREIGN_KEY_CHECKS = 1;
